05e302530330fd6deeb7f23b2a9eaea14c959fda
Your Namealt1tla <GerdzhikovaVeronWench@gmail.com>
2024-08-04 01:38:57 +0400

    edited animation | smthng wrong when tail-to-smthng flip

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 let heads = 0;
 let tails = 0;
-let speed = 5;
+let speed = 1;
+let currentSide = "head";
 let coin = document.querySelector(".coin");
 let flipBtn = document.querySelector("#btn-flip");
 let resetBtn = document.querySelector("#btn-reset");
@@ -14,29 +15,51 @@ function disableButton() {
   flipBtn.disabled = true;
   setTimeout(function () {
     flipBtn.disabled = false;
-  }, 3000);
+  }, speed * 1000);
 }
 
 flipBtn.addEventListener("click", () => {
   let i = Math.random();
   coin.style.animation = "none";
-  if (i < 0.5) {
-    setTimeout(function () {
-      coin.style.animation = `flip-heads ${speed}s forwards`;
-    }, 100);
-    heads++;
+
+  if (currentSide === "head") {
+    if (i < 0.5) {
+      setTimeout(function () {
+        coin.style.animation = `flip-head-to-head ${speed}s forwards`;
+      }, 100);
+      heads++;
+      currentSide = "head";
+    } else {
+      setTimeout(function () {
+        coin.style.animation = `flip-head-to-tail ${speed}s forwards`;
+      }, 100);
+      tails++;
+      currentSide = "tail";
+    }
+    setTimeout(updateStats, speed * 1000);
+    disableButton();
   } else {
-    setTimeout(function () {
-      coin.style.animation = `flip-tails ${speed}s forwards`;
-    }, 100);
-    tails++;
+    if (i < 0.5) {
+      setTimeout(function () {
+        coin.style.animation = `flip-tail-to-head ${speed}s forwards`;
+      }, 100);
+      heads++;
+      currentSide = "head";
+    } else {
+      setTimeout(function () {
+        coin.style.animation = `flip-tail-to-tail ${speed}s forwards`;
+      }, 100);
+      tails++;
+      currentSide = "tail";
+    }
+    setTimeout(updateStats, speed * 1000);
+    disableButton();
   }
-  setTimeout(updateStats, speed *1000);
-  disableButton();
 });
 resetBtn.addEventListener("click", () => {
   coin.style.animation = "none";
   heads = 0;
   tails = 0;
+  currentSide = "head";
   updateStats();
 });
